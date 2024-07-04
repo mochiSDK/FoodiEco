41d10cdbb014621d7fe658ca88bb30a5921d2e60
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-07-04 20:41:07 +0200

    Added custom material theme and typography

--- app/src/main/java/com/fodieco/MainActivity.kt
+++ app/src/main/java/com/fodieco/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.fodieco.ui.theme.FodiEcoTheme
+import com.fodieco.ui.theme.bodyFontFamily
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,6 +36,8 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
+        fontFamily = bodyFontFamily,
+        style = MaterialTheme.typography.bodyLarge,
         modifier = modifier
     )
 }
