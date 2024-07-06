bbbdfee07746f0ee97c0f8505ef6c2fc8f8f74c8
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-07-06 14:14:54 +0200

    Extracted email and password fields into separate composables

--- app/src/main/java/com/fodieco/ui/screens/signin/SignInScreen.kt
+++ app/src/main/java/com/fodieco/ui/screens/signin/SignInScreen.kt
@@ -3,8 +3,6 @@ package com.fodieco.ui.screens.signin
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
@@ -14,9 +12,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import com.fodieco.ui.composables.EmailTextField
+import com.fodieco.ui.composables.PasswordTextField
 import com.fodieco.ui.theme.FodiEcoTheme
 
 @Composable
@@ -29,19 +27,8 @@ fun SignInScreen() {
         modifier = Modifier.fillMaxSize()
     ) {
         Text(text = "Welcome to FoodiEco")
-        OutlinedTextField(
-            value = email,
-            onValueChange = { email = it },
-            label = { Text("Email") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
-        )
-        OutlinedTextField(
-            value = password,
-            onValueChange = { password = it },
-            label = { Text("Password") },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-            visualTransformation = PasswordVisualTransformation()
-        )
+        EmailTextField(email, onValueChange = { email = it })
+        PasswordTextField(password, onValueChange = { password = it})
         Button(onClick = { /*TODO*/ }) {
             Text("Sign In")
         }
