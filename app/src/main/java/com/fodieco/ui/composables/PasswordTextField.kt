97511654b402d020928b1687a971d4667876ffae
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-07-06 15:01:34 +0200

    Added toggle button in password field to show password

--- app/src/main/java/com/fodieco/ui/composables/PasswordTextField.kt
+++ app/src/main/java/com/fodieco/ui/composables/PasswordTextField.kt
@@ -3,25 +3,40 @@ package com.fodieco.ui.composables
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Key
+import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.material.icons.outlined.VisibilityOff
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 
 @Composable
 fun PasswordTextField(password: String, onValueChange: (String) -> Unit, modifier: Modifier = Modifier) {
+    var showPassword by remember { mutableStateOf(false) }
     OutlinedTextField(
         value = password,
         onValueChange = onValueChange,
         label = { Text("Password") },
         leadingIcon = { Icon(Icons.Outlined.Key, contentDescription = "Key icon") },
-        trailingIcon = { Icon(Icons.Outlined.VisibilityOff, contentDescription = "Show password icon") },
+        trailingIcon = {
+            IconButton(onClick = { showPassword = !showPassword }) {
+                when (showPassword) {
+                    true -> Icon(Icons.Outlined.Visibility, contentDescription = "Show password toggle button")
+                    false -> Icon(Icons.Outlined.VisibilityOff, contentDescription = "Show password toggle button")
+                }
+            }
+        },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        visualTransformation = PasswordVisualTransformation(),
+        visualTransformation = if (showPassword) VisualTransformation.None else PasswordVisualTransformation(),
         modifier = modifier
     )
 }
