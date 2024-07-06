199b30a04aed3d354656eb780ab8213da158effb
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-07-06 14:50:55 +0200

    Added icons to email and password text fields

--- app/src/main/java/com/fodieco/ui/composables/PasswordTextField.kt
+++ app/src/main/java/com/fodieco/ui/composables/PasswordTextField.kt
@@ -1,6 +1,10 @@
 package com.fodieco.ui.composables
 
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Key
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -14,6 +18,8 @@ fun PasswordTextField(password: String, onValueChange: (String) -> Unit, modifie
         value = password,
         onValueChange = onValueChange,
         label = { Text("Password") },
+        leadingIcon = { Icon(Icons.Outlined.Key, contentDescription = "Key icon") },
+        trailingIcon = { Icon(Icons.Outlined.VisibilityOff, contentDescription = "Show password icon") },
         keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
         visualTransformation = PasswordVisualTransformation(),
         modifier = modifier
