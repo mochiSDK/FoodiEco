56ad685f8266986acc34f6a6f448f66c4e1a1cad
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-07-08 00:51:53 +0200

    Added settings screen ui

---
+++ app/src/main/java/com/fodieco/ui/screens/settings/SettingsScreen.kt
@@ -0,0 +1,69 @@
+package com.fodieco.ui.screens.settings
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.DarkMode
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.fodieco.ui.theme.FodiEcoTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SettingsScreen() {
+    Scaffold(
+        topBar = {
+            LargeTopAppBar(
+                title = { Text("Settings") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.AutoMirrored.Outlined.ArrowBack, "Back arrow button")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Icon(Icons.Outlined.DarkMode, "Dark mode icon", Modifier.padding(8.dp))
+                Text("Dark theme", modifier = Modifier.padding(8.dp))
+                Spacer(modifier = Modifier.weight(1f))
+                Switch(
+                    checked = false,
+                    onCheckedChange = { /*TODO*/ },
+                    modifier = Modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsPreview() {
+    FodiEcoTheme {
+        SettingsScreen()
+    }
+}
