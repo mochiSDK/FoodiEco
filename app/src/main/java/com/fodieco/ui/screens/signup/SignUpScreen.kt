1fe1b7398cef5536f27a97b502b24bc67a491ae3
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-07-07 18:52:25 +0200

    Added top app bar to sign up screen

--- app/src/main/java/com/fodieco/ui/screens/signup/SignUpScreen.kt
+++ app/src/main/java/com/fodieco/ui/screens/signup/SignUpScreen.kt
@@ -3,8 +3,16 @@ package com.fodieco.ui.screens.signup
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,22 +26,38 @@ import com.fodieco.ui.composables.PasswordTextField
 import com.fodieco.ui.composables.UsernameTextField
 import com.fodieco.ui.theme.FodiEcoTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SignUpScreen() {
     var username by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(text = "Welcome to FoodiEco")
-        UsernameTextField(username, onValueChange = { username = it })
-        EmailTextField(email, onValueChange = { email = it })
-        PasswordTextField(password, onValueChange = { password = it})
-        Button(onClick = { /*TODO*/ }) {
-            Text("Sign Up")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Sign up") },
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.AutoMirrored.Outlined.ArrowBack, "Back arrow button")
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            Text(text = "Welcome to FoodiEco")
+            UsernameTextField(username, onValueChange = { username = it })
+            EmailTextField(email, onValueChange = { email = it })
+            PasswordTextField(password, onValueChange = { password = it})
+            Button(onClick = { /*TODO*/ }) {
+                Text("Sign Up")
+            }
         }
     }
 }
