11cf60fb2d653d51a75106018c7f3c86723653ed
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-12 23:50:09 +0200

    wip: added navigation drawer in home screen

--- app/src/main/java/com/foodieco/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/foodieco/ui/screens/home/HomeScreen.kt
@@ -9,15 +9,22 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowDropDown
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material.icons.outlined.NoFood
 import androidx.compose.material.icons.outlined.Restaurant
 import androidx.compose.material.icons.outlined.RoomService
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
@@ -36,59 +43,89 @@ val chipsPadding = 4.dp
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
-    Scaffold(
-        topBar = {
-            SearchBar(
-                placeholder = { Text("What's in the frige?") },
-                query = "",
-                onQueryChange = {},
-                onSearch = {},
-                active = false,
-                onActiveChange = {},
-                leadingIcon = { IconButton(onClick = { /*TODO*/ }) { Icon(Icons.Outlined.Menu, "Menu icon") } },
-                trailingIcon = {
-                    Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
-                        Icon(Icons.Outlined.Search, "Menu icon")
-                        Spacer(modifier = Modifier.width(14.dp))
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Monogram(text = "A", size = 48.dp, modifier = Modifier.clip(CircleShape))
-                        }
-                    }
-                },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(homeScreenPadding)
-            ) {
-
+    ModalNavigationDrawer(
+        drawerContent = {
+            ModalDrawerSheet {    // TODO: fill icons when item is selected
+                Text("FoodiEco", modifier = Modifier.padding(16.dp))
+                NavigationDrawerItem(
+                    label = { Text("Home") },
+                    icon = { Icon(Icons.Outlined.Home, "Home icon") },
+                    selected = true,
+                    onClick = { /*TODO*/ }
+                )
+                NavigationDrawerItem(
+                    label = { Text("Favorites") },
+                    icon = { Icon(Icons.Outlined.FavoriteBorder, "Favorites icon") },
+                    badge = { /*TODO*/ },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
+                HorizontalDivider()
+                NavigationDrawerItem(
+                    label = { Text("Settings") },
+                    icon = { Icon(Icons.Outlined.Settings, "Settings icon") },
+                    selected = false,
+                    onClick = { /*TODO*/ }
+                )
             }
         }
-    ) { innerPadding ->
-        Column(modifier = Modifier.padding(innerPadding).padding(homeScreenPadding)) {
-            Row {
-                FilterChip(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    label = { Text("Cuisine") },
-                    leadingIcon = { Icon(Icons.Outlined.RoomService, "Cuisine icon") },
-                    trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
-                    modifier = Modifier.padding(chipsPadding)
-                )
-                FilterChip(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    label = { Text("Diet") },
-                    leadingIcon = { Icon(Icons.Outlined.Restaurant, "Diet icon") },
-                    trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
-                    modifier = Modifier.padding(chipsPadding)
-                )
-                FilterChip(
-                    selected = false,
-                    onClick = { /*TODO*/ },
-                    label = { Text("Intolerance") },
-                    leadingIcon = { Icon(Icons.Outlined.NoFood, "Intolerance icon") },
-                    trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
-                    modifier = Modifier.padding(chipsPadding)
-                )
+    ) {
+        Scaffold(
+            topBar = {
+                SearchBar(
+                    placeholder = { Text("What's in the frige?") },
+                    query = "",
+                    onQueryChange = {},
+                    onSearch = {},
+                    active = false,
+                    onActiveChange = {},
+                    leadingIcon = { IconButton(onClick = { /*TODO*/ }) { Icon(Icons.Outlined.Menu, "Menu icon") } },
+                    trailingIcon = {
+                        Row(verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(8.dp)) {
+                            Icon(Icons.Outlined.Search, "Menu icon")
+                            Spacer(modifier = Modifier.width(14.dp))
+                            IconButton(onClick = { /*TODO*/ }) {
+                                Monogram(text = "A", size = 48.dp, modifier = Modifier.clip(CircleShape))
+                            }
+                        }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(homeScreenPadding)
+                ) {
+
+                }
+            }
+        ) { innerPadding ->
+            Column(modifier = Modifier
+                .padding(innerPadding)
+                .padding(homeScreenPadding)) {
+                Row {   // TODO: add horizontal scroll
+                    FilterChip(
+                        selected = false,
+                        onClick = { /*TODO*/ },
+                        label = { Text("Cuisine") },
+                        leadingIcon = { Icon(Icons.Outlined.RoomService, "Cuisine icon") },
+                        trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
+                        modifier = Modifier.padding(chipsPadding)
+                    )
+                    FilterChip(
+                        selected = false,
+                        onClick = { /*TODO*/ },
+                        label = { Text("Diet") },
+                        leadingIcon = { Icon(Icons.Outlined.Restaurant, "Diet icon") },
+                        trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
+                        modifier = Modifier.padding(chipsPadding)
+                    )
+                    FilterChip(
+                        selected = false,
+                        onClick = { /*TODO*/ },
+                        label = { Text("Intolerance") },
+                        leadingIcon = { Icon(Icons.Outlined.NoFood, "Intolerance icon") },
+                        trailingIcon = { Icon(Icons.Outlined.ArrowDropDown, "Arrow drop down icon") },
+                        modifier = Modifier.padding(chipsPadding)
+                    )
+                }
             }
         }
     }
