67db670aca7717d8eb5ac974af0aea1a1db35b1c
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-08-16 00:51:20 +0200

    wip: Added repository with preferences for storing app theme

---
+++ app/src/main/java/com/foodieco/data/repositories/ThemeRepository.kt
@@ -0,0 +1,22 @@
+package com.foodieco.data.repositories
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import com.foodieco.data.models.Theme
+import kotlinx.coroutines.flow.map
+
+class ThemeRepository(private val dataStore: DataStore<Preferences>) {
+    companion object {
+        private val THEME_KEY = stringPreferencesKey("theme")
+    }
+
+    val currentTheme = dataStore.data.map { preferences ->
+        Theme.valueOf(preferences[THEME_KEY] ?: Theme.System.name)    // Making System the default theme.
+    }
+
+    suspend fun setTheme(theme: Theme) {
+        dataStore.edit { preferences -> preferences[THEME_KEY] = currentTheme.toString() }
+    }
+}
--- app/src/main/java/com/foodieco/ui/screens/settings/SettingsViewModel.kt
+++ app/src/main/java/com/foodieco/ui/screens/settings/SettingsViewModel.kt
@@ -1,17 +1,22 @@
 package com.foodieco.ui.screens.settings
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.foodieco.data.models.Theme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
+import com.foodieco.data.repositories.ThemeRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 
 data class ThemeState(val theme: Theme)
 
-class SettingsViewModel : ViewModel() {
-    private val _state = MutableStateFlow(ThemeState(Theme.System))
-    val state = _state.asStateFlow()
+class SettingsViewModel(private val repository: ThemeRepository) : ViewModel() {
+    val state = repository.currentTheme.map { ThemeState(it) }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = ThemeState(Theme.System)
+    )
 
-    fun changeTheme(theme: Theme) {
-        _state.value = ThemeState(theme)
-    }
+    fun changeTheme(theme: Theme) = viewModelScope.launch { repository.setTheme(theme) }
 }
