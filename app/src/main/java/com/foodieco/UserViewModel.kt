8423e1106d4887406faf500d49df60c6a80445c0
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-17 17:45:25 +0200

    Added user view model class

---
+++ app/src/main/java/com/foodieco/UserViewModel.kt
@@ -0,0 +1,48 @@
+package com.foodieco
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.foodieco.data.repositories.UserRepository
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+data class UserState(
+    val username: String = "",
+    val password: String = "",
+    val profilePicture: String = "",
+    val location: String = ""
+)
+
+class UserViewModel(private val repository: UserRepository) : ViewModel() {
+    val state = combine(
+        repository.username,
+        repository.password,
+        repository.profilePicture,
+        repository.location
+    ) { username, password, profilePicture, location ->
+        UserState(username, password, profilePicture, location)
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = UserState()
+    )
+
+    fun setUsername(username: String) = viewModelScope.launch {
+        repository.setUsername(username)
+    }
+
+    fun setPassword(password: String) = viewModelScope.launch {
+        repository.setPassword(password)
+    }
+
+    fun setProfilePicture(picture: R.drawable) = viewModelScope.launch {
+        repository.setProfilePicture(picture)
+    }
+
+    fun setLocation(location: String) = viewModelScope.launch {
+        repository.setLocation(location)
+    }
+
+}
