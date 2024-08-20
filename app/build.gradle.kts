0eaa5c37ce34161a3d8a3e75560020a0190d4fac
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-20 23:50:34 +0200

    Added coil dependency in gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.androidx.datastore.preferences)
     implementation(libs.koin.androidx.compose)
     implementation(libs.play.services.location)
+    implementation(libs.coil.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
