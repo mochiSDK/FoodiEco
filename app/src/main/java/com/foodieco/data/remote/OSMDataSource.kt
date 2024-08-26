e2cfdbfcca5cd39116557861f9aba35c6ffe572d
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-08-26 20:02:49 +0200

    Added data classes for recipe info

--- app/src/main/java/com/foodieco/data/remote/OSMDataSource.kt
+++ app/src/main/java/com/foodieco/data/remote/OSMDataSource.kt
@@ -16,6 +16,48 @@ data class OSMRecipe(
     val image: String,
 )
 
+@Serializable
+data class OSMRecipeInfo(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("servings")
+    val servings: Int,
+    @SerialName("readyInMinutes")
+    val readyInMinutes: Int,
+    @SerialName("cuisines")
+    val cuisines: List<String>,
+    @SerialName("extendedIngredients")
+    val ingredients: List<OSMIngredient>,
+    @SerialName("instructions")
+    val instructions: String,
+    @SerialName("summary")
+    val summary: String
+)
+
+@Serializable
+data class OSMIngredient(
+    @SerialName("id")
+    val id: Int,
+    @SerialName("name")
+    val name: String,
+    @SerialName("measures")
+    val measures: OSMMeasure
+)
+
+@Serializable
+data class OSMMeasure(
+    @SerialName("metric")
+    val metric: OSMUnit
+)
+
+@Serializable
+data class OSMUnit(
+    @SerialName("amount")
+    val amount: Int,
+    @SerialName("unitShort")
+    val unit: String
+)
+
 class OSMDataSource(private val httpClient: HttpClient) {
     private val baseUrl = "https://api.spoonacular.com"
 
@@ -24,6 +66,16 @@ class OSMDataSource(private val httpClient: HttpClient) {
             url {
                 parameters.append("ingredients", ingredient)
                 parameters.append("number", max.toString())
+            }
+        }.body()
+    }
+
+    suspend fun getRecipeInfo(id: Int): OSMRecipeInfo {
+        return httpClient.get("$baseUrl/recipes/$id/information") {
+            url {
+                parameters.append("includeNutrition", "false")
+                parameters.append("addWinePairing", "false")
+                parameters.append("addTasteData", "false")
             }
         }.body()
     }
