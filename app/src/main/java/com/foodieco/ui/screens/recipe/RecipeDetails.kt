2c2e938cbfcf190b399ca298a2daf95ddf224542
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-27 11:34:07 +0200

    Added screen for recipe details

---
+++ app/src/main/java/com/foodieco/ui/screens/recipe/RecipeDetails.kt
@@ -0,0 +1,66 @@
+package com.foodieco.ui.screens.recipe
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
+import androidx.compose.animation.togetherWith
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RecipeDetails(navController: NavHostController) {
+    var isFavorite by remember { mutableStateOf(false) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {},
+                navigationIcon = {
+                    IconButton(onClick = { navController.navigateUp() }) {
+                        Icon(Icons.AutoMirrored.Outlined.ArrowBack, "Arrow back icon")
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { isFavorite = !isFavorite }) {
+                        AnimatedContent(
+                            targetState = isFavorite,
+                            label = "Bouncing scale in transition",
+                            transitionSpec = {
+                                scaleIn(
+                                    animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessMediumLow)
+                                ) togetherWith scaleOut()
+                            }
+                        ) { favorite ->
+                            when {
+                                favorite -> Icon(Icons.Outlined.Favorite, "Favorite icon")
+                                else -> Icon(Icons.Outlined.FavoriteBorder, "Favorite icon")
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+
+        }
+    }
+}
