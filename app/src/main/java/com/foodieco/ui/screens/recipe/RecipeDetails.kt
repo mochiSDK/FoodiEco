3766a0cb63a47f7e63361a8895fab23602b40f5e
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-28 12:52:40 +0200

    Added basic recipe details page

--- app/src/main/java/com/foodieco/ui/screens/recipe/RecipeDetails.kt
+++ app/src/main/java/com/foodieco/ui/screens/recipe/RecipeDetails.kt
@@ -16,23 +16,59 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.foodieco.data.remote.OSMDataSource
+import com.foodieco.data.remote.OSMRecipeDetails
+import com.foodieco.utils.isOnline
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RecipeDetails(
     navController: NavHostController,
     recipeId: String?,
+    osmDataSource: OSMDataSource
 ) {
+    if (recipeId == null) {
+        throw IllegalStateException("Recipe ID was null")
+    }
     var isFavorite by remember { mutableStateOf(false) }
+    var recipe by rememberSaveable { mutableStateOf<OSMRecipeDetails?>(null) }
+    val ctx = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarHostState = remember { SnackbarHostState() }
+
+    LaunchedEffect(Unit) {
+        coroutineScope.launch {
+            if (isOnline(ctx)) {
+                recipe = osmDataSource.searchRecipeById(recipeId.toInt())
+            } else {
+                snackBarHostState.showSnackbar(
+                    message = "An error has occurred while trying to open the recipe",
+                    duration = SnackbarDuration.Long
+                )
+                navController.navigateUp()
+            }
+        }
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -61,10 +97,25 @@ fun RecipeDetails(
                     }
                 }
             )
-        }
+        },
+        snackbarHost = { SnackbarHost(snackBarHostState) }
     ) { innerPadding ->
         Column(modifier = Modifier.padding(innerPadding)) {
-            Text(recipeId.toString())
+            recipe?.let {
+                AsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(it.image)
+                        .build(),
+                    contentDescription = "Recipe image"
+                )
+                Text(it.title)
+                Text("Servings: ${it.servings}")
+                Text("Ready in: ${it.readyInMinutes}")
+                Text("Type: ${it.types}")
+                Text("Ingredients: ${it.ingredients}")
+                Text("Score: ${it.score}")
+                Text("Instructions: ${it.instructions}")
+            }
         }
     }
 }
