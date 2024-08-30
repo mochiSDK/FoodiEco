cd0b595010187f690f55a0ff18037467fd267cf4
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-08-30 13:56:28 +0200

    Removed timeline library dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,7 +85,6 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.jetlime)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
