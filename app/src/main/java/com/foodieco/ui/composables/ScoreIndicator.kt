0adcb4e136462ca16e0c76aacef80c7f57841a6b
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-08-31 13:39:42 +0200

    Improved subtext position in score indicator composable

--- app/src/main/java/com/foodieco/ui/composables/ScoreIndicator.kt
+++ app/src/main/java/com/foodieco/ui/composables/ScoreIndicator.kt
@@ -2,7 +2,6 @@ package com.foodieco.ui.composables
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -19,6 +18,7 @@ import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.foodieco.ui.theme.capriolaFontFamily
 import kotlin.math.roundToInt
 
 @Composable
@@ -28,44 +28,42 @@ fun ScoreIndicator(
     subText: String = "",
     size: Int = 50
 ) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = modifier
-    ) {
-        val color by remember {
-            mutableStateOf(
-                when (value) {
-                    in 0f..33f -> Color(0xFFD84654)
-                    in 34f..66f -> Color(0xFFF99C39)
-                    in 67f..100f -> Color(0xFF4F9D69)
-                    else -> null
-                }
-            )
-        }
-        CircularProgressIndicator(
-            progress = { value.toFloat() / 100 },
-            strokeWidth = (size / 10).dp,
-            strokeCap = StrokeCap.Round,
-            trackColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
-            color = color ?: ProgressIndicatorDefaults.circularColor,
-            modifier = modifier.size(size.dp)
-        )
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = modifier
         ) {
+            val color by remember {
+                mutableStateOf(
+                    when (value) {
+                        in 0f..33f -> Color(0xFFD84654)
+                        in 34f..66f -> Color(0xFFF99C39)
+                        in 67f..100f -> Color(0xFF4F9D69)
+                        else -> null
+                    }
+                )
+            }
+            CircularProgressIndicator(
+                progress = { value.toFloat() / 100 },
+                strokeWidth = (size / 8).dp,
+                strokeCap = StrokeCap.Round,
+                trackColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
+                color = color ?: ProgressIndicatorDefaults.circularColor,
+                modifier = modifier.size(size.dp)
+            )
             Text(
                 "${value.roundToInt()}%",
                 fontSize = size.sp / 4,
                 fontWeight = FontWeight.Bold,
                 color = MaterialTheme.colorScheme.surface
             )
-            if (subText.isNotEmpty()) {
-                Text(
-                    subText,
-                    color = MaterialTheme.colorScheme.surface,
-                    modifier = Modifier.offset(y = (-6).dp)
-                )
-            }
+        }
+        if (subText.isNotEmpty()) {
+            Text(
+                subText,
+                fontFamily = capriolaFontFamily,
+                color = MaterialTheme.colorScheme.surface
+            )
         }
     }
 }
