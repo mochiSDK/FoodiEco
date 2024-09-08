77460dc5cfca56f6afec7776a89b2cb6137e9fb2
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-09-08 13:33:53 +0200

    Added vertical scroll to profile screen

--- app/src/main/java/com/foodieco/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/foodieco/ui/screens/profile/ProfileScreen.kt
@@ -12,6 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.BarChart
@@ -124,6 +126,7 @@ fun ProfileScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             ProfilePictureBox(
                 ctx = ctx,
