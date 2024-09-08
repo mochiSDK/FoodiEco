073eb170f6f574cce26ac296055d2c8e6800f64a
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-09-08 19:36:52 +0200

    Animated progress value in circular indicator

--- app/src/main/java/com/foodieco/ui/composables/CircularIndicator.kt
+++ app/src/main/java/com/foodieco/ui/composables/CircularIndicator.kt
@@ -1,5 +1,8 @@
 package com.foodieco.ui.composables
 
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
@@ -8,6 +11,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ProgressIndicatorDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,13 +34,20 @@ fun CircularIndicator(
     color: Color? = null,
     size: Int = 50
 ) {
+    var startAnimation by remember { mutableStateOf(false) }
+    val progress by animateFloatAsState(
+        targetValue = if (startAnimation) value.toFloat() / 100 else 0f,
+        animationSpec = spring(stiffness = Spring.StiffnessVeryLow),
+        label = "Animated alpha"
+    )
+    LaunchedEffect(Unit) { startAnimation = true }
     Column(horizontalAlignment = Alignment.CenterHorizontally) {
         Box(
             contentAlignment = Alignment.Center,
             modifier = modifier
         ) {
             CircularProgressIndicator(
-                progress = { value.toFloat() / 100 },
+                progress = { progress },
                 strokeWidth = (size / 8).dp,
                 strokeCap = StrokeCap.Round,
                 trackColor = MaterialTheme.colorScheme.surface.copy(alpha = 0.5f),
